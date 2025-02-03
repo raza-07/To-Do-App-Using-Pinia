5216f5ac63fd333de5a1e88d0fd269f0a9e5b7be
raza-07 <alirazaprep07@gmail.com>
2025-02-03 13:28:09 +0500

    Updated Pinia App

---
+++ sec-todo-app/src/components/TodoApp.vue
@@ -0,0 +1,24 @@
+<script setup>
+import TodoFormVue from "./TodoForm.vue";
+import TodoListVue from "./TodoList.vue";
+
+</script>
+
+<template>
+    <div class="todo-app">
+        <h1>To Do App</h1>
+        <TodoFormVue/>
+        <TodoListVue/>
+    </div>
+</template>
+
+<style scoped>
+.todo-app{
+    padding: 5vh;
+    min-height: 100vh;
+    width: 30vw;
+    background-color: #f7f9fc;
+
+}
+
+</style>
